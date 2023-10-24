11c7914bb668e5a002649fa2b0db321a412dd97c
BayuTriwibowo18 <144087130+BayuTriwibowo18@users.noreply.github.com>
2023-10-24 12:42:45 +0700

    Add files via upload

---
+++ ArrayBilangan07_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ArrayBilangan07_1 {
+    public static void main(String[] args){
+        Scanner sc07 =new Scanner(System.in);
+        int[] bil = { 5,17,-7,13};
+
+        for (int i : bil ){
+            System.out.println(bil[i]);
+        }
+ 
+
+       
+
+ {
+    
+}
+}
+}
